f9b64802b89c2fd19ddfdc35abba3e880c7e743d
Julian McOmie <julian.mcomie@gmail.com>
2019-08-16 02:46:43 -0700

    fixed whitespace program, not trying to make it complicated, trying to get the most simple solution possible

--- ReverseCompilers/text_to_whitespace.py
+++ ReverseCompilers/text_to_whitespace.py
@@ -1,13 +1,15 @@
 def text_to_whitespace(text):
-    f = open("whitespace.txt", "w+")
-    f.write("   ")
-    for letter in text:
-        f.write("  ")
-        for binary_character in str(bin(ord(letter))):
-            if binary_character == "0":
-                f.write("s")
-                f.write(" ") #write a space
-            elif binary_character == "1":
-                f.write("\t") #write a tab
-                
-        f.write("\n") #write a line feed
+	f = open("whitespace.txt", "w+")
+	f.write("   ")
+	for letter in text:
+		f.write("  ") #Begin with the stack manipulation Instruction Modification Parameter, then start with the binary number of which to push to the stack
+		for binary_character in str(bin(ord(letter))):
+			if binary_character == "0":
+				f.write(" ") #write a space
+			elif binary_character == "1":
+				f.write("\t") #write a tab
+		f.write("\n") #signify the end of the binary number
+		f.write("\t\n  ") #Go to the Input/Output Instruction Modification Parameter, then simply output the character at the top of the stack
+	f.write("\n"*3) #end of program
+
+text_to_whitespace("Gareth good job the coordinates are ahahaha")
