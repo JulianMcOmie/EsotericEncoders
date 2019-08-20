e6446608bbcb9bd6b2d0006659d2f600f033fd2e
Julian McOmie <julian.mcomie@gmail.com>
2019-08-19 17:54:00 -0700

    added lolcode

---
+++ ReverseCompilers/text_to_lolcode.py
@@ -0,0 +1,4 @@
+def text_to_lolcode():
+	text = input("Text: ")
+	f = open("outputFiles/lolcode.lol", "w+")
+	f.write("""HAI\nVISIBLE "{}"\nKTHXBYE""".format(text, text))
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import os
 from ReverseCompilers.text_to_puberty import text_to_puberty
 from ReverseCompilers.text_to_whitespace import text_to_whitespace
 from ReverseCompilers.text_to_brainfuck import text_to_brainfuck
+from ReverseCompilers.text_to_lolcode import text_to_lolcode
+
+file_extensions = {"brainfuck": ".txt",
+		   "puberty": ".fap",
+		   "whitespace": ".txt",
+		   "lolcode": ".lol"}
 
 exec_list = []
 
@@ -17,4 +23,5 @@ for filename in os.listdir("./ReverseCompilers"):
 
 language_choice = input("\n--> ")
 exec(exec_list[int(language_choice)-1])
-	
+language_name = exec_list[int(language_choice)-1][8:len(exec_list[int(language_choice)-1])-2]
+print("File successfully created in outputFiles/{}".format(language_name + file_extensions[language_name]))
