07712fc9d1a145868f544bbcf5e1db6af38553dc
Julian McOmie <julian.mcomie@gmail.com>
2023-08-21 00:59:26 -0700

    added LolcodeEncoder class

---
+++ encoders/lolcode_encoder.py
@@ -0,0 +1,15 @@
+from encoder import Encoder
+
+
+class LolcodeEncoder(Encoder):
+    def encode(self, text):
+        encoded_text = "\n".join([
+            "HAI",
+            f'VISIBLE "{text}"',
+            "KTHXBYE"
+        ])
+        return encoded_text
+
+    @staticmethod
+    def file_extension():
+        return ".lol"
